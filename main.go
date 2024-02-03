a51a3ae62cbb13eee22971c0043c8f70c112c177
Aidan O'Gorman <aidanogorman@Aidans-MacBook-Air.local>
2024-02-03 17:26:21 +0000

    Check out env vars

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello from your go program")
+	ghRefName := os.Getenv("GITHUB_REF_NAME")
+	ghRefId := os.Getenv("GITHUB_REF")
+	fmt.Printf("Running against ref: %s with ref ID: %s", ghRefName, ghRefId)
 }
